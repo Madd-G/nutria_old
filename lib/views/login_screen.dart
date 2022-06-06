342ffbfa455b1bbb5a60f0801e148d07403baffa
Madd-G <akhmadnuralamsyah@gmail.com>
2022-06-06 18:29:24 +0700

    fixed overflow

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -12,94 +12,96 @@ class LoginScreen extends StatelessWidget {
     final Controller c = Get.put<Controller>(Controller());
     return Scaffold(
         body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 45,
-            ),
-            Text("Login",
-                style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontSize: 40,
-                    fontWeight: FontWeight.w500)),
-            const SizedBox(
-              height: 50,
-            ),
-            const Text(
-              "Enter your email address and\npassword to access your account",
-              style: TextStyle(fontSize: 20),
-            ),
-            const SizedBox(
-              height: 35,
-            ),
-            TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: textInputDecoration.copyWith(
-                    hintText: 'Email',
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 45,
+              ),
+              Text("Login",
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w500)),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                "Enter your email address and\npassword to access your account",
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(
+                height: 35,
+              ),
+              TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: textInputDecoration.copyWith(
+                      hintText: 'Email',
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.primary)))),
+              const SizedBox(height: 20),
+              Obx(
+                () => TextField(
+                  keyboardType: TextInputType.text,
+                  obscureText: c.isHidden.value ? true : false,
+                  decoration: textInputDecoration.copyWith(
+                    suffixIconColor: Theme.of(context).colorScheme.primary,
+                    hintText: 'Password',
                     focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.primary)))),
-            const SizedBox(height: 20),
-            Obx(
-              () => TextField(
-                keyboardType: TextInputType.text,
-                obscureText: c.isHidden.value ? true : false,
-                decoration: textInputDecoration.copyWith(
-                  suffixIconColor: Theme.of(context).colorScheme.primary,
-                  hintText: 'Password',
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Theme.of(context).colorScheme.primary)),
-                  suffixIcon: GestureDetector(
-                    onTap: () => c.toggleVisibility(),
-                    child: Icon(c.isHidden.value == true
-                        ? Icons.visibility_off
-                        : Icons.visibility),
+                            color: Theme.of(context).colorScheme.primary)),
+                    suffixIcon: GestureDetector(
+                      onTap: () => c.toggleVisibility(),
+                      child: Icon(c.isHidden.value == true
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Text('Forgot Password?',
-                  style:
-                      TextStyle(color: Theme.of(context).colorScheme.primary)),
-            ),
-            const SizedBox(
-              height: 70,
-            ),
-            MainButton(
-                color: Theme.of(context).colorScheme.primary,
-                title: 'Login',
-                onPressed: () {
-                  Get.toNamed('/home-screen');
-                }),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Don\'t have an account? ',
-                  style: TextStyle(fontSize: 17),
-                ),
-                GestureDetector(
-                  onTap: () => Get.toNamed('/sign_up_screen_first'),
-                  child: Text('Sign Up',
-                      style: TextStyle(
-                          fontSize: 17,
-                          color: Theme.of(context).colorScheme.primary)),
-                )
-              ],
-            )
-          ],
+              const SizedBox(
+                height: 10,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text('Forgot Password?',
+                    style:
+                        TextStyle(color: Theme.of(context).colorScheme.primary)),
+              ),
+              const SizedBox(
+                height: 70,
+              ),
+              MainButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  title: 'Login',
+                  onPressed: () {
+                    Get.toNamed('/home-screen');
+                  }),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Don\'t have an account? ',
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  GestureDetector(
+                    onTap: () => Get.toNamed('/sign_up_screen_first'),
+                    child: Text('Sign Up',
+                        style: TextStyle(
+                            fontSize: 17,
+                            color: Theme.of(context).colorScheme.primary)),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     ));
--- lib/views/sign_up_screen_first.dart
+++ lib/views/sign_up_screen_first.dart
@@ -17,110 +17,112 @@ class SignUpScreenFirst extends StatelessWidget {
     Get.put(GenderController());
     return Scaffold(
         body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
-            IconButton(
-              icon: const Icon(
-                Icons.arrow_back_ios,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 30,
               ),
-              color: Theme.of(context).colorScheme.primary,
-              onPressed: () {
-                Get.offNamed('/login_screen');
-              },
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Text("Sign Up",
-                style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontSize: 40,
-                    fontWeight: FontWeight.w500)),
-            const SizedBox(
-              height: 30,
-            ),
-            Text(
-              "Please fill information to create an\naccount and sign up to continue"
-                  .tr,
-              style: const TextStyle(fontSize: 20),
-            ),
-            const SizedBox(
-              height: 45,
-            ),
-            TextField(
-                keyboardType: TextInputType.name,
-                decoration: textInputDecoration.copyWith(
-                    hintText: 'Username',
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.primary)))),
-            const SizedBox(height: 20),
-            TextField(
-                keyboardType: TextInputType.streetAddress,
-                decoration: textInputDecoration.copyWith(
-                    hintText: 'Address',
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.primary)))),
-            const SizedBox(height: 20),
-            DropdownSearch<String>(
-                mode: Mode.MENU,
-                showSelectedItem: true,
-                items: languages,
-                label: "Language",
-                hint: "select language",
-                selectedItem: "English",
-                onChanged: (String? data) {
-                  if (data == 'English') {
-                    messageController.changeLanguage('en_US', 'id');
-                  } else if (data == "Indonesia") {
-                    messageController.changeLanguage('id', 'en_US');
-                  }
-                }),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: const [
-                GenderRadioButton(value: 'Male', title: 'Male'),
-                GenderRadioButton(value: 'Female', title: 'Female'),
-              ],
-            ),
-            const SizedBox(
-              height: 70,
-            ),
-            MainButton(
-                color: Theme.of(context).colorScheme.primary,
-                title: 'Next',
-                onPressed: () {
-                  Get.toNamed('/sign_up_screen_second');
-                }),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Don\'t have an account? ',
-                  style: TextStyle(fontSize: 17),
+              IconButton(
+                icon: const Icon(
+                  Icons.arrow_back_ios,
                 ),
-                GestureDetector(
-                  onTap: () => Get.toNamed('/login_screen'),
-                  child: Text('Login',
-                      style: TextStyle(
-                          fontSize: 17,
-                          color: Theme.of(context).colorScheme.primary)),
-                )
-              ],
-            )
-          ],
+                color: Theme.of(context).colorScheme.primary,
+                onPressed: () {
+                  Get.offNamed('/login_screen');
+                },
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text("Sign Up",
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w500)),
+              const SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Please fill information to create an\naccount and sign up to continue"
+                    .tr,
+                style: const TextStyle(fontSize: 20),
+              ),
+              const SizedBox(
+                height: 45,
+              ),
+              TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: textInputDecoration.copyWith(
+                      hintText: 'Username',
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.primary)))),
+              const SizedBox(height: 20),
+              TextField(
+                  keyboardType: TextInputType.streetAddress,
+                  decoration: textInputDecoration.copyWith(
+                      hintText: 'Address',
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.primary)))),
+              const SizedBox(height: 20),
+              DropdownSearch<String>(
+                  mode: Mode.MENU,
+                  showSelectedItem: true,
+                  items: languages,
+                  label: "Language",
+                  hint: "select language",
+                  selectedItem: "English",
+                  onChanged: (String? data) {
+                    if (data == 'English') {
+                      messageController.changeLanguage('en_US', 'id');
+                    } else if (data == "Indonesia") {
+                      messageController.changeLanguage('id', 'en_US');
+                    }
+                  }),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: const [
+                  GenderRadioButton(value: 'Male', title: 'Male'),
+                  GenderRadioButton(value: 'Female', title: 'Female'),
+                ],
+              ),
+              const SizedBox(
+                height: 70,
+              ),
+              MainButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  title: 'Next',
+                  onPressed: () {
+                    Get.toNamed('/sign_up_screen_second');
+                  }),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Don\'t have an account? ',
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  GestureDetector(
+                    onTap: () => Get.toNamed('/login_screen'),
+                    child: Text('Login',
+                        style: TextStyle(
+                            fontSize: 17,
+                            color: Theme.of(context).colorScheme.primary)),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     ));
