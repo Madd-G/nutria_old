c96403bcf34a6ce5fd01fe4cafda201090e823a8
Madd-G <akhmadnuralamsyah@gmail.com>
2022-06-08 18:55:51 +0700

    updated home screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -11,162 +11,177 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Stack(
-          children: [ Column(
-            children: [
-              Container(
-                color: Theme.of(context).colorScheme.primary,
-                height: 250,
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Center(
-                          child: SafeArea(
-                        child: Text(
-                          'Nutria',
+          children: [
+            Column(
+              children: [
+                Container(
+                  color: Theme.of(context).colorScheme.primary,
+                  height: 250,
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Center(
+                            child: SafeArea(
+                          child: Text(
+                            'Nutria',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        )),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        Text(
+                          'Hello Alamsyah,',
                           style: TextStyle(
                               color: Colors.white,
-                              fontSize: 30.0,
-                              fontWeight: FontWeight.w700),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600),
                         ),
-                      )),
-                      SizedBox(
-                        height: 40.0,
-                      ),
-                      Text(
-                        'Hello Alamsyah,',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Text(
-                        "Let\'s Learn More About Fruits and Vegetables",
-                        style: TextStyle(color: Colors.white, fontSize: 15),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 40.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(13.0),
-                      child: Container(
-                        height: 90.0,
-                        width: double.infinity,
-                        color: Theme.of(context).colorScheme.primary,
-                        child: Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Image.asset('assets/images/scan.png'),
-                              const SizedBox(width: 18.0,),
-                              const Text('SCAN UNTUK MENGETAHUI\nJENIS BUAH ATAU SAYURAN', style: TextStyle(
-                                fontSize: 18.0, fontWeight: FontWeight.w700, color: Colors.white
-                              ),)
-                            ],
-                          ),
+                        SizedBox(
+                          height: 20.0,
                         ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 15.0,
-                    ),
-                    const Align(
-                      child: Text('Kategori'),
-                      alignment: Alignment.centerLeft,
-                    ),
-                    const SizedBox(
-                      height: 10.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                          height: 150,
-                          width: 180,
-                          // color: Colors.white,
-                          child: Image.asset('assets/images/buah.png', fit: BoxFit.fill,),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 20.0,
-                        ),
-                        Container(
-                          height: 150,
-                          width: 180,
-                          // color: Colors.white,
-                          child: Image.asset('assets/images/sayuran.png', fit: BoxFit.fill,),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
+                        Text(
+                          "Let\'s Learn More About Fruits and Vegetables",
+                          style: TextStyle(color: Colors.white, fontSize: 15),
                         ),
                       ],
                     ),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    const RecommendationCarousel(),
-                  ],
+                  ),
                 ),
-              )
-            ],
-          ),
+                const SizedBox(
+                  height: 40.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(13.0),
+                        child: Container(
+                          height: 90.0,
+                          width: double.infinity,
+                          color: Theme.of(context).colorScheme.primary,
+                          child: Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Image.asset('assets/images/scan.png'),
+                                const SizedBox(
+                                  width: 18.0,
+                                ),
+                                const Text(
+                                  'SCAN UNTUK MENGETAHUI\nJENIS BUAH ATAU SAYURAN',
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.w700,
+                                      color: Colors.white),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      const Align(
+                        child: Text('Kategori'),
+                        alignment: Alignment.centerLeft,
+                      ),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Container(
+                            height: 150,
+                            width: 180,
+                            // color: Colors.white,
+                            child: Image.asset(
+                              'assets/images/buah.png',
+                              fit: BoxFit.fill,
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 20.0,
+                          ),
+                          Container(
+                            height: 150,
+                            width: 180,
+                            // color: Colors.white,
+                            child: Image.asset(
+                              'assets/images/sayuran.png',
+                              fit: BoxFit.fill,
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      const RecommendationCarousel(),
+                    ],
+                  ),
+                )
+              ],
+            ),
             Positioned(
               top: 220,
-              left: MediaQuery.of(context).size.width/14,
-              right: MediaQuery.of(context).size.width/14,
+              left: MediaQuery.of(context).size.width / 14,
+              right: MediaQuery.of(context).size.width / 14,
               // right: width/2,
               // left: -175,
               child: GestureDetector(
                 onTap: () => Get.toNamed('/login_screen'),
                 child: Container(
-                  height: 60.0,
-                  width: 320.0,
-                  decoration:  BoxDecoration(
-                      color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black,
-                        blurRadius: 1.0,
-                        blurStyle: BlurStyle.outer,
-                        offset: Offset.fromDirection(1.0),
-                        spreadRadius: 1.0
-                      )
-                      ],
-                    borderRadius: BorderRadius.circular(30.0)
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      children: const [
-                        Icon(Icons.search, color: Colors.black26,),
-                        SizedBox(width: 8.0,),
-                        Text('Cari Buah dan Sayuran', style: TextStyle(
-                          color: Colors.black26
-                        ),)
-                      ],
-                    ),
-                  )
-                ),
+                    height: 60.0,
+                    width: 320.0,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black,
+                              blurRadius: 1.0,
+                              blurStyle: BlurStyle.outer,
+                              offset: Offset.fromDirection(1.0),
+                              spreadRadius: 1.0)
+                        ],
+                        borderRadius: BorderRadius.circular(30.0)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.search,
+                            color: Colors.black26,
+                          ),
+                          SizedBox(
+                            width: 8.0,
+                          ),
+                          Text(
+                            'Cari Buah dan Sayuran',
+                            style: TextStyle(color: Colors.black26),
+                          )
+                        ],
+                      ),
+                    )),
               ),
             ),
-    ],
-
+          ],
         ),
       ),
     );
