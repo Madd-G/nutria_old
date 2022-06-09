285a1512ea6253eff3f804c03fa366f1a6dd73c3
Madd-G <akhmadnuralamsyah@gmail.com>
2022-06-09 20:16:31 +0700

    implement recommendations_model

--- lib/components/recommendation_carousel.dart
+++ lib/components/recommendation_carousel.dart
@@ -32,23 +32,67 @@ class RecommendationCarousel extends StatelessWidget {
           ),
         ],
       ),
-      SizedBox(
+      Container(
           height: 200.0,
           child: ListView.builder(
               scrollDirection: Axis.horizontal,
               itemCount: recommendations.length,
               itemBuilder: (BuildContext context, int index) {
-                // Recommendation recommendation = recommendations[index];
+                Recommendation recommendation = recommendations[index];
                 return GestureDetector(
-                  onTap: () => Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => const FruitDetailScreen())),
-                  child: Container(
-                      margin: const EdgeInsets.all(10.0),
-                      width: 200,
-                      height: 150,
-                      color: Theme.of(context).colorScheme.primary,
-                      child: const Text('Check')),
-                );
+                    onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => const FruitDetailScreen(),
+                          ),
+                        ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20.0),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black26,
+                            offset: Offset(0.0, 2.0),
+                            blurRadius: 6.0,
+                          ),
+                        ],
+                      ),
+                      child: Stack(
+                        children: <Widget>[
+                          Hero(
+                            tag: recommendation.imageUrl,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Image(
+                                height: 180.0,
+                                width: 180.0,
+                                image: AssetImage(recommendation.imageUrl),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: 10.0,
+                            bottom: 10.0,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  recommendation.title,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.w600,
+                                    letterSpacing: 1.2,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ));
               }))
     ]);
   }
